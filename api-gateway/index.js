86948bba5dd79ae9372b8e3f10d0716c1b6718a2
Aknexad <aknejad@outlook.com>
2022-11-20 17:39:36 +0330

    add user proxy

---
+++ api-gateway/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+require('dotenv').config();
+const app = express();
+
+// proxy
+app.use(
+  '/user',
+  createProxyMiddleware({
+    target: 'http://localhost:4000/',
+    changeOrigin: true,
+  })
+);
+
+// middleware
+app.use(express.json());
+
+// routes
+
+// server
+app.listen(process.env.PORT, () => console.log('api gateway is up runing'));
