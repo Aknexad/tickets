1686d2903ac14063c3c824f10b40ab4c7f6ec496
Aknexad <aknejad@outlook.com>
2022-11-20 20:11:57 +0330

    add mongoose

--- user-service/index.js
+++ user-service/index.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userModels = require('./models/models');
 
 require('dotenv').config();
 
 const app = express();
 
+// conneect DB
+mongoose.connect('mongodb://localhost:27017/tickets', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected successfully');
+});
+
 // middleware
 app.use(express.json());
 
 // routes
-app.get('/user', (req, res) => {
-  res.send('user page');
+app.get('/user', async (req, res) => {
+  const allUsers = await userModels.find();
+  res.send(allUsers);
+});
+
+app.post('/user/register', async (req, res) => {
+  const user = new userModels(req.body);
+  try {
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 // server
