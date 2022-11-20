330bfdb909a2652fbed84b60073b6278ce7f0134
Aknexad <aknejad@outlook.com>
2022-11-20 23:03:07 +0330

    add  route for get new access toekn

--- auth-server/authServer.js
+++ auth-server/authServer.js
@@ -35,6 +35,22 @@ app.post('/login', authenticationUser, async (req, res) => {
   }
 });
 
+// get new access toekn
+app.post('/accesstoekn', async (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.sendStatus(401);
+  const isInDatabase = await userModels.findOne({ token: refreshToken });
+  if (!isInDatabase) return res.sendStatus(403);
+
+  //   jwt verfy and response new toekn
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = genarateAccessToken(user);
+    res.json({ accessToken: accessToken });
+  });
+});
+
+//
 function genarateAccessToken(payload) {
   return jwt.sign(payload, process.env.ACCESS_TOKEN, { expiresIn: '30s' });
 }
